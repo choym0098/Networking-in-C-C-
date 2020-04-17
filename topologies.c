e3f65f3308d848623646135ec2c6ae2d80e1d4d3
Youngmok Cho <y26cho@uwaterloo.ca>
2020-04-17 00:24:56 -0400

    Add files that create a generic graph that the network is based on

---
+++ topologies.c
@@ -0,0 +1,16 @@
+#include "graph.h"
+
+graph_t *
+build_first_topo() {
+    // Create a graph with three nodes: R0_re, R1_re, R2_re
+    graph_t *topo = create_new_graph("Hello World Generic Graph");
+    node_t *R0_re = create_graph_node(topo, "R0_re");
+    node_t *R1_re = create_graph_node(topo, "R1_re");
+    node_t *R2_re = create_graph_node(topo, "R2_re");
+
+    insert_link_between_two_nodes(R0_re, R1_re, "eth0/0", "eth0/1", 1);
+    insert_link_between_two_nodes(R1_re, R2_re, "eth0/2", "eth0/3", 1);
+    insert_link_between_two_nodes(R0_re, R2_re, "eth0/4", "eth0/5", 1);
+
+    return topo;
+}
