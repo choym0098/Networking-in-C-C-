1af772d692f90d04ca841af8b09c0ada953e5406
Youngmok Cho <y26cho@uwaterloo.ca>
2020-04-17 07:47:09 -0400

    add net.c/net.h - contain data structures/APIs related to network config

---
+++ net.h
@@ -0,0 +1,9 @@
+/*
+net.h contains data structures/APIs relataed to network config
+
+Every node has its own IP address, called as loopback address
+Every interface MUST have mac address, and MAY have ip-address/mask
+*/
+
+
+
