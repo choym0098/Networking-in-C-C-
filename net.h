db500be66184d2922a3a7c346966ca6ddfed5f82
Youngmok Cho <y26cho@uwaterloo.ca>
2020-04-19 22:15:27 -0400

    fix compile error

--- net.h
+++ net.h
@@ -9,8 +9,11 @@ Every interface MUST have mac address, and MAY have ip-address/mask
 #define __NET__
 
 #include <memory.h>
-#include "graph.h"
-#include "util.h"
+#include "utils.h"
+
+typedef struct graph_ graph_t;
+typedef struct node_ node_t;
+typedef struct interface_ interface_t;
 
 typedef struct ip_add_ {
     char ip_addr[16];
@@ -21,6 +24,11 @@ typedef struct mac_add_ {
 } mac_add_t;
 
 typedef struct node_nw_prop_ {
+
+    /* Used to find various device types capabilities of
+     * the node and other features*/
+    unsigned int flags;
+
     // L3 properties
     bool_t is_lb_addr_config;
     ip_add_t lb_addr; // loopback address of node
@@ -50,6 +58,9 @@ init_intf_nw_prop(intf_nw_props_t *intf_nw_props) {
     intf_nw_props->mask = 0;
 }
 
+void
+interface_assign_mac_address(interface_t *interface);
+
 // MAC/IP getter functions
 #define IF_MAC(intf_ptr)    ((intf_ptr)->intf_nw_props.mac_add.mac)
 #define IF_IP(intf_ptr)     ((intf_ptr)->intf_nw_props.ip_add.ip_addr)
@@ -68,6 +79,31 @@ bool_t node_set_loopback_address(node_t *node, char *ip_addr);
 bool_t node_set_intf_ip_address(node_t *node, char *local_if, char *ip_addr, char mask);
 bool_t node_unset_intf_ip_address(node_t *node, char *local_if);
 
+/*
+return the pointer to local interface of a node such that this local
+interface shares the same subnet as that of ip_addr
+*/
+interface_t *
+node_get_matching_subnet_interface(node_t *node, char *ip_addr);
+
+/*
+convert ip address in string form into its equivalent 32-bit integer form
+*/
+unsigned int
+convert_ip_from_str_to_int(char *ip_addr);
+
+/*
+convert ip address in integer form into its equivalent dot decimal form
+*/
+void
+convert_ip_from_int_to_str(unsigned int ip_addr, char *output_buffer);
+
+
+/*
+Functions to dump network information on node/interface properties
+*/
 void dump_nw_graph(graph_t *graph);
+void dump_node_nw_props(node_t *node);
+void dump_intf_props(interface_t *interface);
 
 #endif // __NET__
